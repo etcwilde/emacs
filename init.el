f6ecdeaec8be4beb15e7504e3a0abd58fbd12f6d
Evan Wilde <etcwilde@uvic.ca>
2017-07-26 15:07:53 -0400

    Initial Commit
    Basic initial state
    
    Provides loading of arbitrary configuration files (assuming there is a
    loader for it)
    
    Creates some basic directories for stuff.
    
    Includes org-mode and use-package.

---
+++ init.el
@@ -0,0 +1,27 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Filename $HOME/.emacs.d/initi.el
+;; Author:  Evan Wilde
+;; Date:    July 17, 2017
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Log starting time
+(defvar etcw-el-start-time (current-time) "Time when init.el was started")
+
+;; Root filepath
+(defvar emacs-conf-dir "~/.emacs.d/" "Root configuration Directory")
+(defvar etcw-conf-dir (concat emacs-conf-dir "etcw/") "My configurations")
+
+;; (package-initialize)
+;; (setq package-enable-at-startup nil)"
+
+;; Set paths to org-mode
+;; Different version of org-mode, not elpa
+(add-to-list 'load-path (concat emacs-conf-dir "contrib/org-mode/lisp/"))
+(add-to-list 'load-path (concat emacs-conf-dir "contrib/org-mode/contrib/lisp/"))
+(require 'org)
+
+;; Load up the base functions for everything else
+(org-babel-load-file (concat etcw-conf-dir "base.org"))
+
+;; Show the Time taken to 'boot' emacs
+(message "Configurations started in %f seconds" (float-time (subtract-time (current-time) etcw-el-start-time)))
